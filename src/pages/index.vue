df75b064848ea5ae2e98fc51bf9167b8296ec664
tamuraryoya <tamuraryoya@team-lab.com>
2020-05-06 20:44:21 +0900

    ✨ Add vue environment
    Signed-off-by: tamuraryoya <tamuraryoya@team-lab.com>

---
+++ src/pages/index.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  h1 Hello world.
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+
+@Component
+export default class IndexPage extends Vue {
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
