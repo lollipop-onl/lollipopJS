f0c99eec320933acc3c8c9515932ecfbcd67c4e0
tamuraryoya <tamuraryoya@team-lab.com>
2020-05-06 21:04:45 +0900

    ✨ Add postcss-short
    Signed-off-by: tamuraryoya <tamuraryoya@team-lab.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,12 @@ import { Configuration } from '@nuxt/types';
 
 const config: Configuration = {
   srcDir: 'src',
+  build: {
+    postcss: {
+      plugins: { 'postcss-short': {} },
+    },
+  },
+  buildModules: ['@nuxt/typescript-build'],
 };
 
 module.exports = config;
