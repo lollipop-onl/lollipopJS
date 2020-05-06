1ead875a8aa2674aa762386acd67906a3230fb70
lollipop-onl <lollipop-onl@users.noreply.github.com>
2020-05-06 13:02:45 +0000

    👕 Fixed auto-fixable ESLint errors by github-actions

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,8 +6,7 @@
 import { Component, Vue } from 'nuxt-property-decorator';
 
 @Component
-export default class IndexPage extends Vue {
-}
+export default class IndexPage extends Vue {}
 </script>
 
 <style lang="scss" scoped></style>
