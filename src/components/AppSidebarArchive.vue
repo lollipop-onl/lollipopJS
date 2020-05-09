db72091e4075c85c7cb3d7e2b2cdd9b20a54a35b
lollipop-onl <lollipop-onl@users.noreply.github.com>
2020-05-09 02:21:48 +0000

    👕 Fixed auto-fixable ESLint errors by github-actions

--- src/components/AppSidebarArchive.vue
+++ src/components/AppSidebarArchive.vue
@@ -6,8 +6,7 @@
 import { Component, Vue } from 'nuxt-property-decorator';
 
 @Component
-export default class AppSidebarArchive extends Vue {
-}
+export default class AppSidebarArchive extends Vue {}
 </script>
 
 <style lang="scss" scoped></style>
