90a803ffcff0566cd80c3e86a6c3f40a6aae546b
tamuraryoya <tamuraryoya@team-lab.com>
2020-05-19 19:17:18 +0900

    🔨 Remove webfontloader
    Signed-off-by: tamuraryoya <tamuraryoya@team-lab.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,8 @@
 import { useEffect } from 'react';
 import { AppProps } from 'next/app';
-import dynamic from 'next/dynamic';
+import Head from 'next/head';
 import { debounce } from 'throttle-debounce';
 import './_app.scss';
-
-const WebFont = dynamic(import('../components/WebFont'), { ssr: false });
 
 const App = ({ Component, pageProps }: AppProps) => {
   useEffect(() => {
@@ -26,7 +24,9 @@ const App = ({ Component, pageProps }: AppProps) => {
 
   return (
     <>
-      <WebFont />
+      <Head>
+        <link rel="stylesheet" href="//fonts.googleapis.com/css2?family=DM+Mono:ital,wght@0,300;0,500;1,300;1,500&family=Noto+Sans+JP:wght@300;700&family=Quicksand:wght@400;700&display=swap" />
+      </Head>
       <Component {...pageProps} />
     </>
   );
