d9c881f6f799d4f5bae4076072a24e08d8bb5fe4
tamuraryoya <tamuraryoya@team-lab.com>
2020-05-19 09:42:43 +0900

    🔧 Fix .scss reference error
    Signed-off-by: tamuraryoya <tamuraryoya@team-lab.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import cn from 'classnames';
 import styles from './index.module.scss';
 
 const IndexPage: React.FC<{}> = () => (
   <div className={styles.indexPage}>
     This is home page.
     <span className={styles.foo}>
-      <span className={[styles.message, styles.Variant].join(' ')}>
+      <span className={cn(styles.message, styles.Variant)}>
         This is variant
       </span>
       Hello you.
