fb362222422d04bfcb4c2ff2e1f8870996087753
tamuraryoya <tamuraryoya@team-lab.com>
2020-05-19 17:07:42 +0900

    ✨ Add dynamic --vh variable
    Signed-off-by: tamuraryoya <tamuraryoya@team-lab.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,35 @@
+import { useEffect } from 'react';
 import { AppProps } from 'next/app';
 import dynamic from 'next/dynamic';
+import { debounce } from 'throttle-debounce';
 import './_app.scss';
 
 const WebFont = dynamic(import('../components/WebFont'), { ssr: false });
 
-const App = ({ Component, pageProps }: AppProps) => (
-  <>
-    <WebFont />
-    <Component {...pageProps} />
-  </>
-);
+const App = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    const handleResize = debounce(50, () => {
+      const vh = window.innerHeight * 0.01;
+      const { documentElement } = document;
+
+      documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return (
+    <>
+      <WebFont />
+      <Component {...pageProps} />
+    </>
+  );
+};
 
 export default App;
