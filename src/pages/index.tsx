b34255dc48bbe4c94c62b0c58c54ebc428ca27d3
tamuraryoya <tamuraryoya@team-lab.com>
2020-05-19 09:45:37 +0900

    ⚡️ Update home page text
    Signed-off-by: tamuraryoya <tamuraryoya@team-lab.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,8 @@
 import React from 'react';
-import cn from 'classnames';
 import styles from './index.module.scss';
 
 const IndexPage: React.FC<{}> = () => (
-  <div className={styles.indexPage}>
-    This is home page.
-    <span className={styles.foo}>
-      <span className={cn(styles.message, styles.Variant)}>
-        This is variant
-      </span>
-      Hello you.
-    </span>
-  </div>
+  <div className={styles.indexPage}>This is home page.</div>
 );
 
 export default IndexPage;
