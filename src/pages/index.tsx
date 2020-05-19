5773bb41043ac28056f10757620ed7fcf728d756
tamuraryoya <tamuraryoya@team-lab.com>
2020-05-19 09:22:08 +0900

    ✨ Add CSS markup env
    Signed-off-by: tamuraryoya <tamuraryoya@team-lab.com>

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './index.module.scss';
+
+const IndexPage: React.FC<{}> = () => {
+  return <div className={styles.indexPage}>This is home page. <span className={styles.foo}><span className={[styles.message, styles.Variant].join(' ')}>This is variant</span>Hello you.</span></div>
+};
+
+export default IndexPage;
