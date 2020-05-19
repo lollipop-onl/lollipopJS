61208825e20bb71269ab7ee3990e7a273c25a0b0
tamuraryoya <tamuraryoya@team-lab.com>
2020-05-19 13:40:45 +0900

    ✨ Add webfontloader
    Signed-off-by: tamuraryoya <tamuraryoya@team-lab.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,14 @@
 import { AppProps } from 'next/app';
+import dynamic from 'next/dynamic';
 import './_app.scss';
 
+const WebFont = dynamic(import('../components/WebFont'), { ssr: false });
+
 const App = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <>
+    <WebFont />
+    <Component {...pageProps} />
+  </>
 );
 
 export default App;
