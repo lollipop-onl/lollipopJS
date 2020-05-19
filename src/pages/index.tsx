0df11bb8e66057da3e22f7a6f940b006520ef38f
tamuraryoya <tamuraryoya@team-lab.com>
2020-05-19 09:39:43 +0900

    ✨ Setup ESLint
    Signed-off-by: tamuraryoya <tamuraryoya@team-lab.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import styles from './index.module.scss';
 
-const IndexPage: React.FC<{}> = () => {
-  return <div className={styles.indexPage}>This is home page. <span className={styles.foo}><span className={[styles.message, styles.Variant].join(' ')}>This is variant</span>Hello you.</span></div>
-};
+const IndexPage: React.FC<{}> = () => (
+  <div className={styles.indexPage}>
+    This is home page.
+    <span className={styles.foo}>
+      <span className={[styles.message, styles.Variant].join(' ')}>
+        This is variant
+      </span>
+      Hello you.
+    </span>
+  </div>
+);
 
 export default IndexPage;
