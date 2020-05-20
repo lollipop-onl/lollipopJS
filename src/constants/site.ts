961cc702340cdca3cf49b0927d2d4432fad7de96
tamuraryoya <tamuraryoya@team-lab.com>
2020-05-20 09:05:58 +0900

    ✨ Add Meta tags and post meta informations
    Signed-off-by: tamuraryoya <tamuraryoya@team-lab.com>

--- src/constants/site.ts
+++ src/constants/site.ts
@@ -1,2 +1,8 @@
-/** サイトのタイトル */
+/** Base url of lollipopJS */
+export const SITE_BASE_URL = 'https://js.lollipop.onl'
+
+/** Title of lollipopJS */
 export const SITE_TITLE = 'lollipopJS';
+
+/** Twitter id of simochee */
+export const SITE_TWITTER_ID = '@lollipop_onl';
--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,13 +1,17 @@
 import { FC } from 'react';
 import { GetStaticProps, GetStaticPaths } from 'next';
+import { useRouter } from 'next/router';
 import Head from 'next/head';
 import Link from 'next/link';
 import { Entry } from 'contentful';
+import dayjs from 'dayjs';
+import cn from 'classnames';
 import SiteLayout from '~/components/SiteLayout';
 import AppMarkdown from '~/components/AppMarkdown';
-import { ContentfulContentType } from '~/constants';
+import HeadMeta from '~/components/HeadMeta';
+import { ContentfulContentType, PAGES } from '~/constants';
 import { BlogPost } from '~/types';
-import { contentful, getTitle } from '~/utils';
+import {contentful, getTitle, url} from '~/utils';
 import styles from './[id].module.scss';
 
 type Props = {
@@ -37,17 +41,32 @@ export const getStaticPaths: GetStaticPaths = async () => {
 };
 
 const PostPage: FC<Props> = ({ entry }) => {
-  const { title, body } = entry.fields;
+  const router = useRouter();
+  const { title, image, category, body } = entry.fields;
+  const d = dayjs(new Date(entry.sys.updatedAt));
+  const categoryLink = url(PAGES.CATEGORY_POSTS, { id: category.sys.id, page: 1 });
 
   return (
     <SiteLayout>
       <Head>
         <title>{getTitle(title)}</title>
+        <HeadMeta
+          title={getTitle(title)}
+          path={router.asPath}
+          image={image.fields.file.url}
+        />
       </Head>
       <div className={styles.postPage}>
         <h1 className={styles.title}>
           <span className={styles.inner}>{title}</span>
         </h1>
+        <div className={cn(styles.postMeta, styles.meta)}>
+          <Link href={categoryLink}>
+            <a className={styles.category}>{category.fields.name}</a>
+          </Link>
+          <time dateTime={d.toISOString()} className={styles.date}>{d.format('YYYY/MM/DD h:mm A')}</time>
+        </div>
+        <img className={styles.thumbnail} src={image.fields.file.url} alt={image.fields.title} />
         <AppMarkdown className={styles.body} source={body} />
       </div>
     </SiteLayout>
