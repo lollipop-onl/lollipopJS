26255e5a1d859bb8becbf73b30240dd80cdb3cb6
tamuraryoya <tamuraryoya@team-lab.com>
2020-05-20 09:23:59 +0900

    ⚡️ Update footer
    Signed-off-by: tamuraryoya <tamuraryoya@team-lab.com>

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 require('dotenv').config();
 
+const packageJson = require('./package.json');
+
 module.exports = {
   env: {
+    VERSION: packageJson.version,
     CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
     CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
   },
--- src/components/SiteFooter/index.tsx
+++ src/components/SiteFooter/index.tsx
@@ -9,9 +9,15 @@ type Props = {};
 const SiteFooter: FC<Props> = () => (
   <footer className={styles.siteFooter}>
     <div className={styles.container}>
+      <div className={styles.ga}>
+        このサイトはGoogleアナリティクスを使用しています。
+        <br/>
+        <a className={styles.link} href="https://support.google.com/analytics/answer/4597324" target="_blank" rel="noopener noreferrer">Googleアナリティクスポリシー</a>
+      </div>
       <small className={styles.copyright}>
         &copy; 2020 simochee
       </small>
+      <div className={styles.version}>v{process.env.VERSION}</div>
       <Link href={url(PAGES.DIARY_POSTS, { page: 1 })}>
         <a className={styles.link}>Diary</a>
       </Link>
